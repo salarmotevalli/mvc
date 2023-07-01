39c2f2469149ab513e64307fb733c050baa974c4
salarmotevalli <salar.mo.ro@gmail.com>
2023-07-01 21:16:35 +0330

    comment

--- core/Router.php
+++ core/Router.php
@@ -21,16 +21,29 @@ class Router
 
     protected array $routes = [];
 
+    /**
+     * @return array
+     */
     public function getRoutes(): array
     {
         return $this->routes;
     }
 
+    /**
+     * @param $uri
+     * @param $callback
+     * @return void
+     */
     public function get($uri, $callback): void
     {
         $this->routes['GET'][$uri] = $callback;
     }
 
+    /**
+     * @param $url
+     * @param $callback
+     * @return void
+     */
     public function post($url, $callback): void
     {
         $this->routes['POST'][$url] = $callback;
@@ -38,25 +51,28 @@ class Router
 
     public function resolve()
     {
+        // Get callback by path and request method
         $path = $this->request->getPath();
         $method = $this->request->method();
         $callback = $this->routes[$method][$path];
 
         switch (true) {
+            // Return not found page
             case $callback === null:
                 $this->response->setStatusCode(404);
                 return $this->view->onlyView('_404');
-
+            // Return given view
             case is_string($callback):
                 return $this->view->renderView($callback);
 
+            // Execute controller and return result
             case is_array($callback):
                 Application::$app->controller = new $callback[0]();
                 $callback[0] = Application::$app->controller;
+                return call_user_func($callback, $this->request, $this->response);
 
         }
 
-        return call_user_func($callback, $this->request, $this->response);
     }
 
     public function getResponse(): Response
